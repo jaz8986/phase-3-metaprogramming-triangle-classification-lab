9c96e17dc7ce11c586e0c6254a7a26c7ed77de87
Jaskiran Singh <jsingh8986@yahoo.com>
2023-02-05 17:03:22 -0800

    commit tree e854d5fdb9f994161776ea12967dc8bc15cc4ff1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if [a,b,c].all? {|x| x == 0}
+     raise TriangleError
+    elsif [a,b,c].one? {|x| x < 0}
+      raise TriangleError
+    elsif a + b <= c || b + c <= a || a + c <= b
+      raise TriangleError
+    else
+      if a == b && b == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
+
+binding.pry
+0
